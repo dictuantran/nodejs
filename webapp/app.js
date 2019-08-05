cdebc325229ed1fa32268a4a3f0db5e393cc6416
TUAN TRAN <dictuantran@gmail.com>
2019-08-06 00:20:49 +0700

    mysql-crud

--- webapp/app.js
+++ webapp/app.js
@@ -1,45 +1,15 @@
-var path = require("path");
-var express = require("express");
-var zipdb = require("zippity-do-dah");
-var ForecastIo = require("forecastio");
+var mysql = require('mysql');
 
-var app = express();
-var weather = new ForecastIo("YOUR FORECAST.IO API KEY OR DARKSKY.NET API KEY HERE");
-
-app.use(express.static(path.resolve(__dirname, "public")));
-
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.get("/", function(req, res) {
-  res.render("index");
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Aa123456"
 });
 
-app.get(/^\/(\d{5})$/, function(req, res, next) {
-  var zipcode = req.params[0];
-  var location = zipdb.zipcode(zipcode);
-  if (!location.zipcode) {
-    next();
-    return;
+con.connect(function(err) {
+  if(err) {
+    throw err;
   }
 
-  var latitude = location.latitude;
-  var longitude = location.longitude;
-
-  weather.forecast(latitude, longitude, function(err, data) {
-    if (err) {
-      next();
-      return;
-    }
-
-    res.json({
-      zipcode: zipcode,
-      temperature: data.currently.temperature
-    });
-  });
-});
-
-app.use(function(req, res) {
-  res.status(404).render("404");
-});
-app.listen(3000);
+  console.log('Connected!');
+})
